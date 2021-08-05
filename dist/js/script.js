93ebb0b6fe7727cb52e05c6cd554071bf845215f
Ifeoluwa Olaiya <ifeoluwa1201@gmail.com>
2021-08-05 17:58:05 +0100

    update

---
+++ dist/js/script.js
@@ -0,0 +1,20 @@
+
+AOS.init({
+    duration: 1000,
+});
+
+//Smooth scrolling
+const links = document.querySelectorAll(".nav-ul li a");
+
+for (const link of links) {
+    link.addEventListener('click', smoothScroll);
+}
+
+function smoothScroll(e) {
+    e.preventDefault();
+
+    const href = this.getAttribute("href");
+    document.querySelector(href).scrollIntoView({
+        behaviour: "smooth",
+    });
+}
